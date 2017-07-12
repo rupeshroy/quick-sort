a8ccab14903a3a3c79b0389177a96d945794e879
Rupesh Roy <Rupesh_Roy@epam.com>
2017-07-12 13:37:44 +0530

    Implementation

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -1,6 +1,33 @@
 'use strict';
 
 function quickSort(input) {
+if(input.constructor ===Array)
+{
+	if(!input.length)	
+	{
+		if(input.length ==1)
+		 {
+			 return input;
+		 }
+		 else{
+			 if(input.length >1 && typeof(input[0])=='Number')
+			 {
+				 return input.sort(function(a, b){return a-b});
+			 }
+			 else
+			 {
+				  return input.sort();
+			 }
+		 }
+	} 
+	 else 
+	 {
+		 return;
+		}
+}
+else{
+	throw Error("not an array");
+}
 
 };
 module.exports = quickSort;
